2f60ba8525469a68248f68ce2e743fc06f2557d1
amazurenko <neo-katso@yandex.ru>
2017-03-03 01:48:26 +0300

    rem

--- src/main/java/mtp/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package mtp.repository;
-
-import mtp.domain.Order;
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.UUID;
-
-public interface OrderRepository extends CrudRepository<Order, UUID>, OrderRepositoryCustom {}
--- src/main/java/mtp/repository/OrderRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package mtp.repository;
-
-import  mtp.domain.Order;
-
-import java.util.List;
-
-public interface OrderRepositoryCustom {
-    List<Order> findByStore(String store);
-}
