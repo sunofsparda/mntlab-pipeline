71a4951a9697b48b3ec092051e3b92f6f08ce19d
“Andrei <Andrei_Kutsko1@epam.com>
2017-03-05 15:04:30 +0300

    1.0

---
+++ gradle_install.sh
@@ -0,0 +1,15 @@
+# installs to /opt/gradle
+# existing versions are not overwritten/deleted
+# seamless upgrades/downgrades
+# $GRADLE_HOME points to latest *installed* (not released)
+gradle_version=3.3
+wget -N https://services.gradle.org/distributions/gradle-${gradle_version}-all.zip
+mkdir /opt/gradle
+mkdir /opt/gradle/latest
+unzip gradle-${gradle_version}-all.zip -d /opt/gradle/
+ln -sfn gradle-${gradle_version} /opt/gradle/latest/
+printf "export GRADLE_HOME=/opt/gradle/latest\nexport PATH=\$PATH:\$GRADLE_HOME/bin" > /etc/profile.d/gradle.sh
+sudo chmod +x /etc/profile.d/gradle.sh
+. /etc/profile.d/gradle.sh
+# check installation
+gradle -v
